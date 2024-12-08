a7d1b8623903d33dd3245bbdb81c7778983755f1
mataisomi@gmail.com <mataisomi@gmail.com>
2024-12-08 21:17:11 +0100

    Session storage

---
+++ scripts/products.js
@@ -0,0 +1,29 @@
+Cola.onclick= function (){
+    window.sessionStorage.setItem("cola", "Coca Cola");
+};
+redbull.onclick= function (){
+    window.sessionStorage.setItem("redbull", "Red Bull");
+};
+nordpole.onclick= function (){
+    window.sessionStorage.setItem("nordpole", "Nordpole");
+};
+julmust.onclick= function (){
+    window.sessionStorage.setItem("julmust", "Julmust");
+};
+delicatoboll.onclick= function (){
+    window.sessionStorage.setItem("delicatoboll","Delicatoboll");
+};
+haribo.onclick= function (){
+    window.sessionStorage.setItem("haribo","Haribo Peach");
+};
+kexchoklad.onclick= function (){
+    window.sessionStorage.setItem("kexchocklad","Kexchocklad");
+};
+barbell.onclick= function (){
+    window.sessionStorage.setItem("barbell","Barbell Protein Bar");
+};
+
+
+
+
+
