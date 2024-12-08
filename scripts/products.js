0e33e26a383a4b71d081d05d3c22b3f1498f3d26
kndynow <139381069+kndynow@users.noreply.github.com>
2024-12-08 13:10:07 +0100

    Add: script that get product data fron json and displays them in specified section
    - Added script that fetch data from dummy json-file with products.
        - Stores the product data in separate variables for separate categories
        - Displays products under specified category-sections

--- scripts/products.js
+++ scripts/products.js
@@ -1,19 +1,55 @@
-//dummy json
-const jsonFile = "../products.json";
-
-//fetch product data from dummy api
-async function fetchData() {
+// fetch product data from dummy api/product-database
+async function fetchData(jsonFile) {
   try {
     const response = await fetch(jsonFile);
     if (!response.ok) {
       throw new Error("Could not fetch data.");
     }
     const data = await response.json();
+    return data;
   } catch (error) {
     console.error(error);
   }
 }
 
-fetch(jsonFile)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+// Get data from json and stores them in separate categories
+async function createProducts() {
+  const jsonFile = "products.json";
+  const data = await fetchData(jsonFile);
+
+  if (!data) return;
+
+  const snacks = data.products.snacks;
+  const drinks = data.products.drinks;
+
+  //Display categories under their category
+  displayProduct(snacks, "snacks_section");
+  displayProduct(drinks, "drinks_section");
+}
+
+// Display products takes product categories and ID for targeted section as argument
+function displayProduct(products, productSectionID) {
+  const productContainer = document.getElementById(productSectionID);
+
+  if (!productContainer) return;
+
+  productContainer.innerHTML = products
+    .map(
+      (product) =>
+        `
+      <div class="product-card stacked">
+        <img
+        class="product-image"
+        src="${product.imageUrl}"
+        alt="${product.name}" />
+        <div class="product-info">
+          <h3 class="product-name">${product.name}</h3>
+          <p class="product-price">Pris: ${product.price}</p>
+        </div>
+        </div>
+      `
+    )
+    .join("");
+}
+
+createProducts();
